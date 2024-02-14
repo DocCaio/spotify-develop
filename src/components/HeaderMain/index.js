0d65d4fcc2b2dab1a76a02987eea8f5f49cc1423
Caio <108128119+DocCaio@users.noreply.github.com>
2024-02-14 14:53:10 -0300

    up

--- src/components/HeaderMain/index.js
+++ src/components/HeaderMain/index.js
@@ -3,18 +3,18 @@ import './Header-main.css';
 const Header = () => {
     return (
         <main>
-    <div class="main-container">
+    <div className="main-container">
       /* INVERTER AS CLASSES */
-      <nav class="header__navigation">
-        <div class="navigation">
-          <button class="arrow-left">
+      <nav className="header__navigation">
+        <div className="navigation">
+          <button className="arrow-left">
             <img src="./icons/small-left.png" alt="" />
           </button>
-          <button class="arrow-right">
-            <img src="./src/assets/icons/small-right.png" alt="" />
+          <button className="arrow-right">
+            <img src="./icons/small-right.png" alt="" />
           </button>
 
-          <div class="header__search">
+          <div className="header__search">
             <img src="./src/assets/icons/search.png" alt="" />
             <input id="search-input" maxlength="800" autocorrect="off" autocapitalize="off" spellcheck="false"
               placeholder="O que você quer ouvir?" value="" />
@@ -22,140 +22,140 @@ const Header = () => {
         </div>
 
         <div class="header__login">
-          <button class="subscribe">Inscreva-se</button>
-          <button class="login">Entrar</button>
+          <button className="subscribe">Inscreva-se</button>
+          <button className="login">Entrar</button>
         </div>
       </nav>
 
       /* PLAYLIST */
-      <div class="playlist-container">
+      <div className="playlist-container">
         <div id="result-playlists">
-          <div class="playlist">
+          <div className="playlist">
           /* COLOCAR O BOM DIA | BOA TARDE | BOA NOITE */
             <h1 id="greeting"></h1>
-            <h2 class="session">Navegar por todas as seções</h2>
+            <h2 className="session">Navegar por todas as seções</h2>
           </div>
 
           /* LIST ITEM 1 */
-          <div class="offer__scroll-container">
-            <div class="offer__list">
-              <section class="offer__list-item">
+          <div className="offer__scroll-container">
+            <div className="offer__list">
+              <section className="offer__list-item">
                 /* CARD 1 */
-                <a href="" class="cards">
-                  <div class="cards card1">
-                    <img src="./src/assets/playlist/1.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card1">
+                    <img src="/playlist/1.jpeg" alt="" />
                     <span>Boas festas</span>
                   </div>
                 </a>
 
                /* CARD 2 */
-                <a href="" class="cards">
-                  <div class="cards card2">
-                    <img src="./src/assets/playlist/2.png" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card2">
+                    <img src="/playlist/2.png" alt="" />
                     <span>Made for you</span>
                   </div>
                 </a>
 
                 /* CARD 3 */
-                <a href="" class="cards">
-                  <div class="cards card3">
-                    <img src="./src/assets/playlist/3.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card3">
+                    <img src="/playlist/3.jpeg" alt="" />
                     <span>Realeases</span>
                   </div>
                 </a>
 
                 /* CARD 4 */
-                <a href="" class="cards">
-                  <div class="cards card4">
-                    <img src="./src/assets/playlist/4.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card4">
+                    <img src="/playlist/4.jpeg" alt="" />
                     <span>Creators</span>
                   </div>
                 </a>
 
                 /* CARD 5 */
-                <a href="" class="cards">
-                  <div class="cards card5">
-                    <img src="./src/assets/playlist/5.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card5">
+                    <img src="/playlist/5.jpeg" alt="" />
                     <span>Para treinar</span>
                   </div>
                 </a>
 
                 /* CARD 6 */
-                <a href="" class="cards">
-                  <div class="cards card6">
-                    <img src="./src/assets/playlist/6.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card6">
+                    <img src="/playlist/6.jpeg" alt="" />
                     <span>Podcasts</span>
                   </div>
                 </a>
 
                 /* CARD 7 */
-                <a href="" class="cards">
-                  <div class="cards card7">
-                    <img src="./src/assets/playlist/7.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card7">
+                    <img src="/playlist/7.jpeg" alt="" />
                     <span>Sertanejo</span>
                   </div>
                 </a>
 
                /* CARD 8 */
-                <a href="" class="cards">
-                  <div class="cards card8">
-                    <img src="./src/assets/playlist/8.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card8">
+                    <img src="/playlist/8.jpeg" alt="" />
                     <span>Samba e pagode</span>
                   </div>
                 </a>
 
                 /* CARD 9 */
-                <a href="" class="cards">
-                  <div class="cards card9">
-                    <img src="./playlist/9.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card9">
+                    <img src="/playlist/9.jpeg" alt="" />
                     <span>Funk</span>
                   </div>
                 </a>
 
                 /* CARD 10 */
-                <a href="" class="cards">
-                  <div class="cards card10">
-                    <img src="./playlist/10.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card10">
+                    <img src="/playlist/10.jpeg" alt="" />
                     <span>MPB</span>
                   </div>
                 </a>
 
                 /* CARD 11 */
-                <a href="" class="cards">
-                  <div class="cards card11">
-                    <img src="./playlist/11.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card11">
+                    <img src="/playlist/11.jpeg" alt="" />
                     <span>Rock</span>
                   </div>
                 </a>
 
               /* CARD 12 */
-                <a href="" class="cards">
-                  <div class="cards card12">
-                    <img src="./playlist/12.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card12">
+                    <img src="/playlist/12.jpeg" alt="" />
                     <span>Hip Hop</span>
                   </div>
                 </a>
 
                 /* CARD 13 */
-                <a href="" class="cards">
-                  <div class="cards card13">
-                    <img src="./playlist/13.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card13">
+                    <img src="/playlist/13.jpeg" alt="" />
                     <span>Indie</span>
                   </div>
                 </a>
 
                 /* CARD 14 */
-                <a href="" class="cards">
-                  <div class="cards card14">
-                    <img src="./src/assets/playlist/14.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card14">
+                    <img src="/playlist/14.jpeg" alt="" />
                     <span>Relax</span>
                   </div>
                 </a>
 
                 /* CARD 15 */
-                <a href="" class="cards">
-                  <div class="cards card15">
-                    <img src="./src/assets/playlist/15.jpeg" alt="" />
+                <a href="" className="cards">
+                  <div className="cards card15">
+                    <img src="/playlist/15.jpeg" alt="" />
                     <span>Música Latina</span>
                   </div>
                 </a>
@@ -166,18 +166,18 @@ const Header = () => {
 
 
         <div id="result-artist" class="hidden">
-          <div class="grid-container">
-            <div class="artist-card" id="">
-              <div class="card-img">
+          <div className="grid-container">
+            <div className="artist-card" id="">
+              <div className="card-img">
                 <img id="artist-img" class="artist-img" />
-                <div class="play">
-                  <span class="fa fa-solid fa-play"></span>
+                <div className="play">
+                  <span className="fa fa-solid fa-play"></span>
                 </div>
               </div>
-              <div class="card-text">
+              <div className="card-text">
                 <a title="Foo Fighters" class="vst" href="">
-                <span class="artist-name" id="artist-name"></span>
-                <span class="artist-categorie">Artista</span>
+                <span className="artist-name" id="artist-name"></span>
+                <span className="artist-categorie">Artista</span>
                 </a>
               </div>
             </div>
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,15 +2,15 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer class="disclaimer-premium">
-        <div class="text">
-          <p class="disclaimer-premium__title">Preview of Spotify</p>
-          <p class="disclaimer-premium__subtitle">
+        <footer className="disclaimer-premium">
+        <div className="text">
+          <p className="disclaimer-premium__title">Preview of Spotify</p>
+          <p className="disclaimer-premium__subtitle">
             Sing up to get unlimited songs and podcasts with
             occasional ads.Nocreat card neeeded.
           </p>
         </div>
-        <div class="button">
+        <div className="button">
           <button type="button">Sing up free</button>
         </div>
       </footer>
